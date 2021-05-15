06a12acbd97a31ebde11f6dd90c4aaa5144b2f07
raghu3111x <74596785+raghu3111x@users.noreply.github.com>
2021-05-15 13:36:19 +0000

    Add files via upload

---
+++ cheat.py
@@ -0,0 +1,29 @@
+from docx import Document
+import PyPDF2
+import pyfiglet
+import webbrowser
+import time 
+
+# doc = Document('sample.docx')
+
+# for para in doc.paragraphs:
+# 	print(para.text)
+
+pdf_file_object = open('Chemistry.pdf','rb')
+pdf_reader = PyPDF2.PdfFileReader(pdf_file_object)
+n = pdf_reader.numPages
+for i in range(n):
+	print(pyfiglet.figlet_format(text='Page Number   ' + str(i+1)))
+	print('\n')
+	global page_obj,text
+	page_obj = pdf_reader.getPage(i)
+	text = page_obj.extractText()
+	ques = text.split('?')
+	num = len(ques)
+	for a in range(num):
+		webbrowser.open(url='https://www.google.com/search?q=' + ques[a])
+	_next = input(':')
+	if _next.lower() =='next':
+		pass
+	else:
+		time.sleep(10000000000)
